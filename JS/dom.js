37a7bfdbda98c724442759c454ec21159791836a
BoikoMaxim <122735165m@gmail.com>
2021-03-06 20:12:43 +0200

    homework js

--- JS/dom.js
+++ JS/dom.js
@@ -140,14 +140,14 @@ function clockStop() {
 clockStart();
 
 document.getElementById('hider').onclick = function() {
-    document.getElementById('text').hidden = true;
+  document.getElementById('text').hidden = true;
 }
 
 let menuElem = document.getElementById('sweeties');
 let titleElem = menuElem.querySelector('.title');
 
 titleElem.onclick = function() {
-    menuElem.classList.toggle('open');
+  menuElem.classList.toggle('open');
 };
 
 let panes = document.querySelectorAll('.pane');
@@ -155,4 +155,90 @@ let panes = document.querySelectorAll('.pane');
   for(let pane of panes) {
     pane.insertAdjacentHTML("afterbegin", '<button class="remove-button">[x]</button>');
     pane.firstChild.onclick = () => pane.remove();
+}
+
+let selectedOption = genres.options[genres.selectedIndex];
+alert( selectedOption.value );
+
+let newOption = new Option("Классика", "classic");
+genres.append(newOption);
+
+newOption.selected = true;
+
+function showCover() {
+  let coverDiv = document.createElement('div');
+  coverDiv.id = 'cover-div';
+
+  // убираем возможность прокрутки страницы во время показа модального окна с формой
+  document.body.style.overflowY = 'hidden';
+
+  document.body.append(coverDiv);
+}
+
+function hideCover() {
+  document.getElementById('cover-div').remove();
+  document.body.style.overflowY = '';
+}
+
+function showPrompt(text, callback) {
+  showCover();
+  let form = document.getElementById('prompt-form');
+  let container = document.getElementById('prompt-form-container');
+  document.getElementById('prompt-message').innerHTML = text;
+  form.text.value = '';
+
+  function complete(value) {
+    hideCover();
+    container.style.display = 'none';
+    document.onkeydown = null;
+    callback(value);
   }
+
+  form.onsubmit = function() {
+    let value = form.text.value;
+    if (value == '') return false; // игнорируем отправку пустой формы
+
+    complete(value);
+    return false;
+  };
+
+  form.cancel.onclick = function() {
+    complete(null);
+  };
+
+  document.onkeydown = function(e) {
+    if (e.key == 'Escape') {
+      complete(null);
+    }
+  };
+
+  let lastElem = form.elements[form.elements.length - 1];
+  let firstElem = form.elements[0];
+
+  lastElem.onkeydown = function(e) {
+    if (e.key == 'Tab' && !e.shiftKey) {
+      firstElem.focus();
+      return false;
+    }
+  };
+
+  firstElem.onkeydown = function(e) {
+    if (e.key == 'Tab' && e.shiftKey) {
+      lastElem.focus();
+      return false;
+    }
+  };
+
+  container.style.display = 'block';
+  form.elements.text.focus();
+}
+
+document.getElementById('show-button').onclick = function() {
+  showPrompt("Введите что-нибудь<br>...умное :)", function(value) {
+    alert("Вы ввели: " + value);
+  });
+};
+
+showPrompt("Введите что-нибудь<br>...умное :)", function(value) {
+  alert(value);
+});
